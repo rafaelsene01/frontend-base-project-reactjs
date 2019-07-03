3a7e0fe57418345085d06c4bf0f5f616f326310c
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-03 16:38:47 -0300

    base project 2.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./App.css";
-
-import TechList from "./components/TechList";
+import React from 'react';
 
 function App() {
-  return <TechList />;
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import App from './App';
 
-render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('root'));
