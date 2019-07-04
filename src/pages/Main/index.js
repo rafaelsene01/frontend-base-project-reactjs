2cffb1c8a92b24f4295c8ae509c6f9f6f599d93d
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-04 11:42:38 -0300

    finalizado projeto base

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import { Container, SubmitButton, Form, List } from './styles';
+import { SubmitButton, Form, List } from './styles';
+import Container from '../../components/Container';
 
 export default class Main extends Component {
   state = {
