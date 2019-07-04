6e97b5a85609da9a4009085d6bc3be792b8fcb2f
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-04 00:54:17 -0300

    adicionando funcionalidades no css

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Title } from './styles';
+import { FaGitAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-export default function Main() {
-  return (
-    <Title error={false}>
-      Hello World <small>menor</small>
-    </Title>
-  );
+import api from '../../services/api';
+
+import { Container, SubmitButton, Form } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  hendleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGitAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.hendleInputChange}
+            value={newRepo}
+            placeholder="Adicionar repositorio"
+          />
+
+          <SubmitButton loading={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
